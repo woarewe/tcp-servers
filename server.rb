09d46ba140d82f38598b4b9e4b9858fd87eb5479
woarewe <rostislav.zhuravsky@gmail.com>
2022-11-24 13:40:14 +0100

    Add thread pool as a solution

--- server.rb
+++ server.rb
@@ -2,10 +2,12 @@
 
 require 'socket'
 require 'logger'
+require 'concurrent-ruby'
 
 PORT = 2000
 LIMIT = 1_000
 BYTES = 100 * 1024 * 1024
+CORES_NUMBER = 10
 
 class Server
   attr_reader :port, :server, :logger
@@ -14,16 +16,24 @@ class Server
     @port = port
     @server = TCPServer.new(port)
     @requests = 0
-    @logger = Logger.new($stdout)
+  end
+
+  def count_request
+    @requests += 1
+  end
+
+  def requests
+    @requests
   end
 end
 
 class Serial < Server
   def start
+    logger = Logger.new($stdout)
     loop do
       handle_request(server.accept)
-      @requests += 1
-      logger.info(@requests)
+      count_request
+      logger.info(requests)
     end
   end
 
@@ -35,8 +45,31 @@ class Serial < Server
   end
 end
 
+class ThreadPool < Server
+  def start
+    logger = Logger.new($stdout)
+    workers = Concurrent::FixedThreadPool.new(CORES_NUMBER)
+    loop do
+      handle_request(workers, server.accept)
+      count_request
+      logger.info(requests)
+    end
+  end
+
+  def handle_request(workers, connection)
+    workers.post do
+      connection.read(BYTES)
+      data = File.read(File.expand_path('./data.txt', __dir__))
+      connection.write(data)
+      connection.close
+    end
+  end
+end
+
+
 server = case ARGV[0]
 when 'serial' then Serial.new(PORT)
+when 'thread-pool' then ThreadPool.new(PORT)
 end
 
 server.start
